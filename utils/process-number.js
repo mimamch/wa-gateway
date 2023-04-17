03002b2c1578a40c2e1cb6d0f8c0c341a3b3a05b
Muhammad Imam Choirudin <38609616+mimamch@users.noreply.github.com>
2023-04-17 11:42:32 +0700

    Update process-number.js

--- utils/process-number.js
+++ utils/process-number.js
@@ -2,7 +2,7 @@ exports.processNumber = (number) => {
   let finalNumber = number;
   if (!number.includes("@s.whatsapp.net")) number = number + "@s.whatsapp.net";
   if (number[0] == "6" && number[1] == "2") return number;
-  if (number[0] == "+" && number[1] == "6" && number[2] == ["2"])
+  if (number[0] == "+" && number[1] == "6" && number[2] == "2")
     return number.substring(1);
   if (number[0] == "0" && number[1] == "8") {
     let deleted = number.substring(1);
