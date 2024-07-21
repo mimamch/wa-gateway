c9957633b6afa30bcec8b993dc62c2997a88441c
fajri <fajri@gariskode.com>
2024-07-21 08:11:19 +0700

    add feature send image

--- app/routers/message_router.js
+++ app/routers/message_router.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const {
   sendMessage,
   sendBulkMessage,
+  sendImageMessage
 } = require("../controllers/message_controller");
 const MessageRouter = Router();
 
 MessageRouter.all("/send-message", sendMessage);
 MessageRouter.all("/send-bulk-message", sendBulkMessage);
+MessageRouter.all("/send-image", sendImageMessage);
 
 module.exports = MessageRouter;
